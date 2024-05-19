f14c6417fe3233e2f0e72daebb5ff0a2bbf37f95
elahe919 <elahehbeigi512@gmail.com>
2024-05-19 17:28:39 +0330

    just create boxes

---
+++ vite-project/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Main() {
+  return (
+    <>
+      <div className=" flex justify-between [&>*]:bg-indigo-600 [&>*]:w-60 [&>*]:h-60 [&>*]:m-10 [&>*]:rounded-lg">
+        <div className="">
+            <div>
+                
+            </div>
+        </div>
+        <div className=""></div>
+      </div>
+      <button className="p-5 bg-black text-white w-1/2 mt-10">Rolling</button>
+    </>
+  );
+}
+
+export default Main;
